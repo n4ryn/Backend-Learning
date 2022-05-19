7fac495dd474bd65788b2a675f2f806b28980e46
n4ryn <vinaykumar.cs19@aryacollege.in>
2022-05-19 21:53:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+
+const path = require("path");
+
+const express = require("express");
+const { fileURLToPath } = require("url");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "index.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.get("/restaurants", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.get("/recommend", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "recommend.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.post("/recommend", function (req, res) {
+  const restaurent = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurents = JSON.parse(fileData);
+
+  storedRestaurents.push(fileData);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurents));
+
+  res.redirect("/confirm");
+});
+
+app.get("/confirm", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "confirm.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.get("/about", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "about.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.listen(3000);
